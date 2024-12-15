b4a2661879572e6ae7a8911ec2d783e0fc2c634c
RadwanHegazy <radwangaber22@gmail.com>
2024-12-15 09:31:30 +0200

    edit admin panel

--- dj_auth_package/admin.py
+++ dj_auth_package/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from .models import ResetPasswordModel
-
-@admin.register(ResetPasswordModel)
-class ResetForm (admin.ModelAdmin) : 
-    list_display = ['id','created_at','finished_at', 'user']
